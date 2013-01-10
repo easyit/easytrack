dc97c91ba69c1c629ac1b76d368a3ea74262217d
easyit <easy.it@qq.com>
2013-01-10 13:51:53 +0800

    2013-01-10

--- easyFleet/app.js
+++ easyFleet/app.js
@@ -4,12 +4,27 @@
  */
 
 var express = require('express')
+//Mongo Store
+  , mongoStore = require('connect-mongodb') 
+  , fmsConfig = require('./config')
+  , Db = require('mongodb').Db
+  , Server = require('mongodb').Server
+  , server_config = new Server(fmsConfig.mongodb.host, fmsConfig.mongodb.port, {auto_reconnect: true, native_parser: true})
+  , sessionDb = new Db(fmsConfig.mongodb.db, server_config, {})
+//
+  , mongo = require('mongoskin')
+  // , fmsDb = mongo.db(fmsConfig.mongodb.host + ':' + fmsConfig.mongodb.port + '/' + fmsConfig.mongodb.db + '?auto_reconnect')
+ // 
   , routes = require('./routes')
+//  , logins = require('./routes/login')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
+global.fmsDb = mongo.db(fmsConfig.mongodb.host + ':' + fmsConfig.mongodb.port + '/' + fmsConfig.mongodb.db + '?auto_reconnect');
+
 var app = express();
+
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -18,7 +33,15 @@ app.configure(function(){
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: 'easyfms',
+    store: new mongoStore({db: sessionDb, collection: 'user_sessions'})
+  }));
+
   app.use(express.methodOverride());
+  // app.use(express.csrf());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -28,6 +51,10 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+//app.get('/login',  routes.login);
+app.post('/login', routes.loginP);
+app.get('/home', routes.home);
+//app.get('/logout', logins.logout);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- easyFleet/routes/index.js
+++ easyFleet/routes/index.js
@@ -1,8 +1,72 @@
+// 'use strict'
+
+var AM = require('../modules/account-manager');
 
 /*
- * GET home page.
+ * GET root page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	console.log('Serving request for url [GET] ' + req.route.path);
+
+	// check if the user's credentials are saved in a cookie //
+	if (req.cookies.user == undefined || req.cookies.pass == undefined){
+		res.render('login', {  title: 'Hello - Please Login To Your Account' });
+	}else{
+	// attempt automatic login //
+		AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+			if (o != null){
+				req.session.user = o;
+				res.redirect('/home');
+			}	else{
+				res.render('login',  { title: 'Hello - Please Login To Your Account' });
+			}
+		});
+	}
 };
+
+
+exports.loginP = function(req, res){
+	console.log( req.body.user );
+	console.log( req.body.pass );
+
+	// if (req.session.user) {
+	// 	res.redirect('/home');
+	// }
+	
+	AM.manualLogin(req.body.user, req.body.pass, function(e, o){
+			if (!o){
+				console.log("login error !!!!");
+				res.send(e, 400);
+			}	else{
+			    req.session.user = o;
+				if (req.param('remember-me') == 'true'){
+					res.cookie('user', o.user, { maxAge: 900000 });
+					res.cookie('pass', o.pass, { maxAge: 900000 });
+				}
+				res.send(o, 200);
+			}
+		});
+};
+
+
+/*
+ * GET logged-in user homepage 
+ */
+exports.home = function(req, res){
+	if (req.session.user == null){
+	// if user is not logged-in redirect back to login page //
+		res.redirect('/');
+	}   else{
+		res.render('home', {
+				locals: {
+					title : 'Control Panel',
+					countries : CT,
+					udata : req.session.user
+				}
+			});
+	}
+};
+
+
+
