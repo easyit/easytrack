8707ac6fe127ec02400a66c8a70df84570b53ca1
easyit <easy.it@qq.com>
2013-01-13 01:26:07 +0800

    demo v0.3

--- easyFleet/public/js/views/dashboard/vmd.js
+++ easyFleet/public/js/views/dashboard/vmd.js
@@ -1,7 +1,16 @@
 
 $(document).ready(function(){
     var logoutController = new LogoutController();
-    $('#vehicles_table').dataTable();
+
+
+
+    $('#vehicles_table').dataTable( {
+    	"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+    	"sPaginationType": "bootstrap",
+		"oLanguage": {
+			"sLengthMenu": "_MENU_ records per page"
+		}
+	} );
 
 
     
--- easyFleet/public/js/views/home.js
+++ easyFleet/public/js/views/home.js
@@ -3,52 +3,63 @@ $(document).ready(function(){
 
 	var logoutController = new LogoutController();
 
-	drawPollingStaus();
 	drawViolationCount();
-	drawViolationSummary();
 
-	//$.plot($("#dash_polling_status"),  [ [[0, 0], [1, 1]] ], { yaxis: { max: 1 } });
+	$.ajax({
+            url: '/data/polling_status.json',
+            method: 'GET',
+            dataType: 'json',
+            success: onPollingStatusDataReceived
+        });
 
-	function pieHover(event, pos, obj) {
-            if (!obj)
-                return;
-            percent = parseFloat(obj.series.percent).toFixed(2);
-            //percent = obj.series.data[0][1].toString();
-            $("#hover").html('<br/><br/><span style="font-weight: bold; color: ' + obj.series.color + '">' + obj.series.label + ' (' + percent + '%)</span>');
-        }
+	$.ajax({
+            url: '/data/violation_summary.json',
+            method: 'GET',
+            dataType: 'json',
+            success: onViolationSummaryDataReceived
+        });
 
-    function pieClick(event, pos, obj) {
-            if (!obj)
-                return;
-            percent = parseFloat(obj.series.percent).toFixed(2);
-            var n;
-            n = obj.series.label.indexOf('Inactive')
-            if ( n >= 0 )
-                {
-                    var reurl = "Reports/ListPage.aspx?fromdate=2010.11.06 00:00:00&todate=2010.11.06 00:00:00&strException=1&strMonth=1&strYear=2012&strgroup=1&vehicleno=''&strDriverId=&strGeofenceID=&interval=&reportname=rpt_getInactiveVehicles&reportheader=Inactive Vehicles&strModuleID=45&strCompanyID=30&strFRID=1"
-                    window.location.replace(reurl);
-                }
-            else {
-                    alert('' + obj.series.label + ': ' + percent + '%');
-            }
-        }
+	// then fetch the data with jQuery
+    function onPollingStatusDataReceived(series) {
+			drawPollingStatus(series);
+    }
+        
+    
+    function onViolationSummaryDataReceived(data) {
+    
+    	drawViolationSummary(data);
+    }
 
-	function drawPollingStaus() {
-            var data = [];
-            var series = 4 //Math.floor(Math.random()*10)+1;
-            var paramEmployeeId = 2075
-            var paramDashboard = 1
+    function drawViolationSummary(vsdata){
+		var pVar1Title = 'Oct - 2012';
+		var pVar2Title = 'Nov - 2012';
+		var pVar3Title = 'Dec - 2012';
 
-            data[0] = { label: "Moving   - ", data: 4, color: 'green' }
-            data[1] = { label: "Idle     - ", data: 5, color: 'yellow' }
-            data[2] = { label: "Stop     - ", data: 9, color: 'red' }
-            data[3] = { label: "Inactive - ", data: 6, color: 'gray' }
+     	var options = {
+     		series: {
+     			lines: { show: true },
+     			points: { show: true }
+     		},
+     		legend: { show: true,  noColumns:5, container: '#legendholder' },
+     		xaxis: { tickDecimals: 0, ticks: [0, [1, pVar1Title], [2, pVar2Title], [3, pVar3Title]] },
+     		yaxis: { min: 0 },
+     		selection: { mode: "x" }
+     	};
 
-            // INTERACTIVE
-            $.plot($("#interactive"), data,
-	        {
-	            series: {
-	                pie: {
+     	var placeholder = $("#dbViolationSummary");
+     	var plot = $.plot(placeholder, vsdata, options);
+     } 
+
+     function drawPollingStatus(series){
+     	var data = [	{ label: "Moving   - ", data: 4, color: 'green' },
+				{ label: "Idle     - ", data: 5, color: 'yellow' },
+				{ label: "Stop     - ", data: 9, color: 'red' },
+				{ label: "Inactive - ", data: 6, color: 'gray' }
+			]
+
+        var options = {
+	        series: {
+	            pie: {
 	                	//innerRadius: 0.2,
 	                    show: true,
 	                    radius: 1,
@@ -71,11 +82,18 @@ $(document).ready(function(){
 	            //     hoverable: true,
 	            //     clickable: true
 	            // }
-	        });
+	        }
+            
+            // and plot all we got
+            $.plot($("#interactive"), data, options);
 
-            $("#interactive").bind("plothover", pieHover);
-            $("#interactive").bind("plotclick", pieClick);
-        }
+     }
+
+    
+
+	
+
+	
 
     function drawViolationCount(){
 
@@ -148,55 +166,8 @@ $(document).ready(function(){
 
  
 
-    function drawViolationSummary(){
+    
 
-
-		var pVar1 = 1
-		var pVar2 = 2
-		var pVar3 = 3
-		var pVar1Title = 'Oct - 2012'
-		var pVar2Title = 'Nov - 2012'
-		var pVar3Title = 'Dec - 2012'
-		var vsdata = [
-		{
-		label: 'OVERSPEED',
-		data: [[pVar1, 512], [pVar2, 925], [pVar3, 1623]]
-		},
-		{
-		label: 'HARSH BRAKE',
-		data: [[pVar1, 378], [pVar2, 971], [pVar3, 1249]]
-		},
-		{
-		label: 'UNAUTORISED STOP',
-		data: [[pVar1, 38], [pVar2, 123], [pVar3, 117]]
-		},
-		{
-		label: 'IDLING',
-		data: [[pVar1, 191], [pVar2, 538], [pVar3, 733]]
-		},
-		{
-		label: 'CONT DRIVE',
-		data: [[pVar1, 4], [pVar2, 5], [pVar3, 7]]
-		}
-		];
-
-
-     	var options = {
-     		series: {
-     			lines: { show: true },
-     			points: { show: true }
-     		},
-     		legend: { show: true,  noColumns:5, container: '#legendholder' },
-     		xaxis: { tickDecimals: 0, ticks: [0, [1, pVar1Title], [2, pVar2Title], [3, pVar3Title]] },
-     		yaxis: { min: 0 },
-     		selection: { mode: "x" }
-     	};
-
-     	var placeholder = $("#dbViolationSummary");
-     	var plot = $.plot(placeholder, vsdata, options);
-     } 
-
-     function drawVehicleSummary(){
-     }
+     
 	 
 })
