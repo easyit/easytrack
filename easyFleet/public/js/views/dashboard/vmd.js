4630e737802ea677552ceda7cc1c3c818503b700
easyit <easy.it@qq.com>
2013-01-12 16:19:17 +0800

    demo v0.2

---
+++ easyFleet/public/js/views/dashboard/vmd.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    var logoutController = new LogoutController();
+    $('#vehicles_table').dataTable();
+
+
+    
+    // $('#top_overspeed_table').dataTable( 
+    //  {"bPaginate": false,
+ //        "bLengthChange": false,
+ //        "bFilter": true,
+ //        "bSort": false,
+ //        "bInfo": false,
+ //        "bAutoWidth": false} );
+})
